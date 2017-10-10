0f17d54b90075ef5a5e78066f523c332e64c3089
Nicollette Karaivanova <nicollette@Genius.local>
2017-10-10 22:17:25 +0300

    renamed project

--- Pods/Target Support Files/Pods-XMPPChatClient/Pods-XMPPChatClient-umbrella.h
+++ Pods/Target Support Files/Pods-XMPPChatClient/Pods-XMPPChatClient-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_XMPPVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_XMPPVersionString[];
+FOUNDATION_EXPORT double Pods_XMPPChatClientVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_XMPPChatClientVersionString[];
 
